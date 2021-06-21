9efe710c5b1b82f7b3971d9af1c4297f9a49ecd7
Juris Gribuska <juris.gribuska@bt.com>
2021-06-21 09:37:59 +0300

    completed

--- rna_transcription.rb
+++ rna_transcription.rb
@@ -1,8 +1,12 @@
-=begin
-Write your code for the 'Rna Transcription' exercise in this file. Make the tests in
-`rna_transcription_test.rb` pass.
+class Complement
+  MAPPING = {
+    "G" => "C",
+    "C" => "G",
+    "T" => "A",
+    "A" => "U"
+  }
 
-To get started with TDD, see the `README.md` file in your
-`ruby/rna-transcription` directory.
-=end
-
+  def self.of_dna dna
+    dna.split('').map {|v| v = MAPPING[v]}.join
+  end
+end
